a95bbd27201a108b862606a8e76a3cc1c12eebad
Jesse Mace <jesmac87@gmail.com>
2014-09-03 19:16:03 -0600

    implementation of the black diamond jS libraries.

--- js/animations.js
+++ js/animations.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
+	
 	$('.tweet-compose').on('click' ,  function(){
 		//show the text counter and the tweet button when clicking text area
 		$(this).closest('#tweet-content').find('#tweet-controls').show();
 		//double the amount of text space availble to tweet
 		$(this).closest('#tweet-content').find('.tweet-compose').css({'height' : '5em'});
 	});
-
 
 	//modify the char count so user knows how many characters are left
 	$('.tweet-compose').on('keyup', function(){
@@ -30,6 +30,8 @@ $(document).ready(function(){
 		 };
 	});
 
+
+
 	//create a new tweet when user clicks tweet.
 	$('#tweet-submit').on('click', function(){
 		var message = $('.tweet-compose').val();
@@ -40,17 +42,50 @@ $(document).ready(function(){
 		//reset to default color
 		$(this).closest('#tweet-content').find('#char-count').css('color' , '#999');
 		//create new tweet
-		$('#stream').prepend('<div class = "tweet"><div class="content"><img class="avatar" src="img/Jessem Face.png"><strong class="fullname">Jesse Mace</strong><span class="username"> @JMDevMnt</span><p class="tweet-text"></p></div></div>' );
+		$('#stream').prepend('<div class = "tweet"> ' +
+								'<div class="content">' +
+									'<img class="avatar" src="img/Jessem Face.png">' +
+									'<strong class="fullname">Jesse Mace</strong>' +
+									'<span class="username"> @JMDevMnt</span>' +
+									'<p class="tweet-text"></p>' +
+								'</div>' +
+							'</div>' );
 		//add the reply, tweet, favorite, more actions to tweet.
-		$('#stream	div:first').find('.content').append('<div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div>')
+		$('#stream	div:first').find('.content').append('<div class="tweet-actions">'+
+															'<ul>'+
+																'<li><span class="icon action-reply"></span> Reply</li>'+
+																'<li><span class="icon action-retweet"></span> Retweet</li>'+
+																'<li><span class="icon action-favorite"></span> Favorite</li>'+
+																'<li><span class="icon action-more"></span> More</li>'+
+															'</ul>'+
+														'</div>')
+		//add the stats to the tweet
+		$('#stream	div:first').find('.content').append('<div class="stats">'+
+															'<div class="retweets"><p class="num-retweets">0</p><p>RETWEETS</p></div>'+
+															'<div class="favorites"><p class="num-favorites">0</p><p>FAVORITES</p></div>'+
+															'<div class="users-interact">'+
+																'<div><img class="thumbnail" data-toggle="tooltip" title="Jesse Mace" src="img/JesseM Face.png">'+
+																'<img src="img/vklimenko.jpg"></div>'+
+															'</div>'+
+															'<div class="timeago" title=""></div>'+
+														'</div>');
 		//add the message to the newly created tweet
 		$('#stream div:first').find('.tweet-text').prepend(message);
+		//timeago
+		var date = new Date().toISOString();
+		$('.timeago').attr('title' , date);
+		$('.timeago').timeago();
 		
 	});
 
 	//Show/hide the stats div for the tweet
-	$('.content').on('click', function(){
-		$(this).find('.stats').animate({height: "toggle"}, 1000);
+	$(document.body).on('click', '.content', function(){
+		$(this).find('.stats').animate({height: "toggle"}, 800);
+	});
+
+	//Bootstrap tooltip integration
+	$(document.body).on('mouseover', '.thumbnail', function(){
+		$(this).tooltip();
 	});
 
 });
